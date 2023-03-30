94f9128544f46b97cc07798d5090e778dba830f4
abhinass <kabhinav403@gmail.com>
2023-03-30 11:57:20 +0530

    [Abhinav] Add: Shell program to Store the results in a Dictionary for every Computation

--- maths.sh
+++ maths.sh
@@ -7,5 +7,16 @@ read b
 echo "Enter Value of c: "
 read c
 
-sum=$((a%b+c))
-echo "The value of a%b+c is: $sum"
+sum1=$((a+b+c))
+sum2=$((a+b*c))
+sum3=$((a*b+c))
+sum4=$((c+a/b))
+sum5=$((a%b+c))
+
+declare -A sums
+sums[sum1]="$((a+b+c))"
+sums[sum2]="$((a+b*c))"
+sums[sum3]="$((a*b+c))"
+sums[sum4]="$((c+a/b))"
+sums[sum5]="$((a%b+c))"
+
